c13759614a91e123c03756b476ba2114aedc19de
shepijcanwu <shepijcanwu@tencent.com>
2022-05-06 14:51:29 +0800

    feat: 测试import 是否有效

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ export * as Line from './line'
 export * as Rect from './rect'
 export { getRelativePoint } from './positionCalculate';
 export * as Matrix from './matrix'
-export { getElementDocumentRect } from './docPositionCalculate'
+import { getElementDocumentRect } from './docPositionCalculate'
+export const a = 'aaa'
+
+export {
+  getElementDocumentRect
+}
