d0e7acae815d2fbed5edd9880e26ee9ea0aa1378
shepijcanwu <shepijcanwu@tencent.com>
2022-05-07 10:54:22 +0800

    feat: change types

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,15 +1,63 @@
-type Position = Global.Position
-type ElementRect = Global.ElementRect
-type Point = Global.Point
-type MatrixType = Global.MatrixType
-type LineEquation = Global.LineEquation
-type PointOnLineEquation = Global.PointOnLineEquation;
-type Size = Global.Size;
-type Direction = Global.Direction;
-type LineDirection = Global.LineDirection;
-type ApexAngleDirection = Global.ApexAngleDirection;
-
 declare namespace Graphics {
+  type CanWritable<T> = {
+    -readonly [k in keyof T]: T[k]
+  };
+
+  enum PointOnLineEquation {
+    On = 'on',
+    Down = 'down',
+    Up = 'up',
+  }
+
+  interface Position {
+    left: number;
+    top: number;
+  }
+
+  interface Point {
+    x: number;
+    y: number;
+  }
+
+  interface Size {
+    width: number;
+    height: number
+  }
+
+
+  interface MatrixType {
+    translateX: number,
+    translateY: number,
+    rotate: number,
+    skew: number,
+    scaleX: number,
+    scaleY: number
+  }
+
+  type ElementRect = CanWritable<Omit<DOMRect, 'toJSON'>>;
+
+  interface LineEquation {
+    A: number;
+    B: number;
+    C: number;
+  }
+
+  // 顶角方向
+  type ApexAngleDirection  = 'topRight'
+  | 'bottomRight'
+  | 'bottomLeft'
+  | 'topLeft';;
+
+  // 四边方向
+  type LineDirection = 'top'
+  | 'right'
+  | 'bottom'
+  | 'left';
+
+  type Direction = ApexAngleDirection | LineDirection;
+
+  type DirectionType = 'apex-angle' | 'line';
+
   function getElementDocumentRect(element: HTMLElement, position: Position): ElementRect
 
   function getRelativePoint(
@@ -57,5 +105,6 @@ declare namespace Graphics {
   };
 }
 
-export = Graphics & Global
-export default Graphics & Global;
+export = Graphics
+export as namespace Graphics
+export default Graphics;
